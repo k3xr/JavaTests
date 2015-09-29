656b4a708f826f6bf0b1c2c9a371a8aebdc43bac
k3xr <k3ther@gmail.com>
2015-09-30 00:06:01 +0200

    Statistics simple tests

--- Statistics/src/Statistics.java
+++ Statistics/src/Statistics.java
@@ -5,17 +5,6 @@
  */
 public class Statistics {
 
-//	public static Collection<Integer> data;
-//	
-//	public static double average(Collection<Double> data){
-//
-//		double average = 0;
-//		for (Iterator<Double> iterator = data.iterator(); iterator.hasNext();) {
-//			average += (double) iterator.next();;
-//	    }
-//		return average/data.size();		
-//	}	
-
 	public static double average(double[] data){
 
 		double average = 0;
@@ -26,28 +15,26 @@ public class Statistics {
 	}	
 
 	/**
-	 * 
 	 * @param data
 	 * @return histogram of the data as a string
 	 */
 	public static String histogram(double[] data){
 
 		double max = data[0];
+		double min = data[0];
 		for (int i=1; i<data.length; i++){
 			if (data[i] > max){
 				max = data[i];
 			}
-		}
-		double min = data[0];
-		for (int i=1; i<data.length; i++){
-			if (data[i] < min){
+			else if (data[i] < min){
 				min = data[i];
 			}
 		}
-		int numberOfClases = (int)Math.sqrt(data.length)+1;
-		double classLength = (max - min)/numberOfClases;
+		
+		int numberOfClasses = (int)Math.sqrt(data.length)+1;
+		double classLength = (max - min)/numberOfClasses;
 
-		int[] freq = new int[numberOfClases];
+		int[] freq = new int[numberOfClasses];
 
 		for (int i=0; i<data.length; i++){
 			// for each value
@@ -55,10 +42,7 @@ public class Statistics {
 			double minInterval = min;
 			double maxInterval = min+classLength;
 			
-//			System.out.println("*----*");
-			
-			for(int j=0; j<numberOfClases; j++){
-//				System.out.println("value "+ data[i] + " max: "+ maxInterval +" min: " + minInterval);
+			for(int j=0; j<numberOfClasses; j++){
 				// for each class
 				if(j==0 && data[i] >= minInterval && data[i] <= maxInterval){
 					freq[0]++;
@@ -71,9 +55,7 @@ public class Statistics {
 				minInterval = maxInterval;
 				maxInterval = maxInterval+classLength;
 			}
-		}
-		
-		String toPrint = "";
+		}		
 		
 		int maxFreq = freq[0];
 		for(int i=1; i<freq.length; i++){
@@ -82,41 +64,53 @@ public class Statistics {
 			}
 		}
 		
+		String toPrint = "";
 		for(int i=0; i<maxFreq;){
-			toPrint = toPrint+"     ";
-			for(int j=0; j<numberOfClases; j++){
+			toPrint += "    ";
+			for(int j=0; j<numberOfClasses; j++){
 				if(freq[j] >= maxFreq){
-					toPrint = toPrint+"*     ";
+					toPrint = insertOcurrence(toPrint);
 				}
 				else{
-					toPrint = toPrint+"      ";
+					toPrint = insertNonOcurrence(toPrint);
 				}
-				toPrint = toPrint+"   ";
 			}
 			maxFreq--;
-			toPrint = toPrint+"\n";
+			toPrint += "\n";
 		}		
 		
-		for(int j=0; j<numberOfClases; j++){
-			// for each class			
-			toPrint = toPrint + "_________";
-		}	
-		toPrint = toPrint + "\n"; 
+		toPrint = printBase(toPrint, numberOfClasses);		
 		
-		for(int j=0; j<numberOfClases; j++){
+		for(int j=0; j<numberOfClasses; j++){
 			// for each class			
-			toPrint = toPrint + "|        ";
+			toPrint += (min+classLength*j)+"\t"; 						
 		}	
-		toPrint = toPrint + "|\n"; 
+		toPrint += max + "\n";
 		
-		for(int j=0; j<numberOfClases; j++){
+		return toPrint;
+	}
+	
+	private static String insertOcurrence(String toPrint){
+		return toPrint += "*       ";
+	}
+	
+	private static String insertNonOcurrence(String toPrint){
+		return toPrint += "        ";
+	}
+	
+	private static String printBase(String toPrint, int numberOfClasses){
+		
+		for(int j=0; j<numberOfClasses; j++){
 			// for each class			
-			toPrint = toPrint + (min+classLength*j)+"     "; 						
+			toPrint += "________";
 		}	
-		toPrint = toPrint + max + "\n"; 
-			
-		System.out.println(toPrint);
+		toPrint += "\n"; 
 		
-		return "asd";
+		for(int j=0; j<numberOfClasses; j++){	
+			toPrint += "|       ";
+		}	
+		toPrint += "|\n"; 	
+		
+		return toPrint;		
 	}
 }
---
+++ Statistics/test/TestStatistics.java
@@ -0,0 +1,126 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestStatistics {
+
+	@Test
+	public void average2_3returns4(){
+		
+		double[] anArray = new double[2];
+		anArray[0] = 3;
+		anArray[1] = 5;
+		// 3rd argument is precision
+		assertEquals(4.0, Statistics.average(anArray),0.1);
+	}
+	
+	@Test
+	public void average5_0_10returns5(){
+		
+		double[] anArray = new double[3];
+		anArray[0] = 5;
+		anArray[1] = 0;
+		anArray[2] = 10;
+		assertEquals(5.0, Statistics.average(anArray),0.1);
+	}
+	
+	@Test
+	public void average10returns10(){
+		
+		double[] anArray = new double[1];
+		anArray[0] = 10;
+		assertEquals(10, Statistics.average(anArray),0.1);
+	}
+	
+	@Test
+	public void histogram10_2_10(){
+		
+		double[] anArray = new double[3];
+		anArray[0] = 10;
+		anArray[1] = 2;
+		anArray[2] = 10;
+		assertEquals(
+				  "            *       \n"
+				+ "    *       *       \n"
+				+ "________________\n"
+				+ "|       |       |\n"
+				+ "2.0\t6.0\t10.0\n", 
+				Statistics.histogram(anArray));
+	}
+	
+	@Test
+	public void histogram15Data(){
+		
+		double[] anArray = new double[15];
+		anArray[0] = 5;
+		anArray[1] = 6;
+		anArray[2] = 14;
+		anArray[3] = 10;
+		anArray[4] = 2;
+		anArray[5] = 1;
+		anArray[6] = 6;
+		anArray[7] = 7;
+		anArray[8] = 6;
+		anArray[9] = 5;
+		anArray[10] = 10;
+		anArray[11] = 6;
+		anArray[12] = 6;
+		anArray[13] = 5;
+		anArray[14] = 5;
+		assertEquals(
+				  "            *                       \n"
+				+ "            *                       \n"
+				+ "            *                       \n"
+				+ "            *                       \n"
+				+ "            *                       \n"
+				+ "            *                       \n"
+				+ "            *                       \n"
+				+ "            *                       \n"
+				+ "    *       *       *               \n"
+				+ "    *       *       *       *       \n"
+				+ "________________________________\n"
+				+ "|       |       |       |       |\n"
+				+ "1.0\t4.25\t7.5\t10.75\t14.0\n", Statistics.histogram(anArray));
+	}
+	
+	@Test
+	public void histogram20Data(){		
+		double[] anArray = new double[20];
+		anArray[0] = 3;
+		anArray[1] = 1;
+		anArray[2] = 1;
+		anArray[3] = 1;
+		anArray[4] = 2;
+		anArray[5] = 2;
+		anArray[6] = 2;
+		anArray[7] = 2;
+		anArray[8] = 3;
+		anArray[9] = 3;
+		anArray[10] = 3;
+		anArray[11] = 4;
+		anArray[12] = 4;
+		anArray[13] = 4;
+		anArray[14] = 3;
+		anArray[15] = 4;
+		anArray[16] = 4;
+		anArray[17] = 4;
+		anArray[18] = 4;
+		anArray[19] = 4;
+		assertEquals(
+				  "                                    *       \n"
+				+ "                                    *       \n"
+				+ "                                    *       \n"
+				+ "                            *       *       \n"
+				+ "            *               *       *       \n"
+				+ "    *       *               *       *       \n"
+				+ "    *       *               *       *       \n"
+				+ "    *       *               *       *       \n"
+				+ "________________________________________\n"
+				+ "|       |       |       |       |       |\n"
+				+ "1.0\t1.6\t2.2\t2.8\t3.4\t4.0\n", Statistics.histogram(anArray));
+	}
+	
+	
+}
