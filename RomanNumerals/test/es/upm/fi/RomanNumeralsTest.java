afe3e45e5ad899fa531ae01b1a187062340c442f
k3xr <k3ther@gmail.com>
2015-10-11 14:31:16 +0200

    Tests cases to parameterized test class

--- RomanNumerals/test/es/upm/fi/RomanNumeralsTest.java
+++ RomanNumerals/test/es/upm/fi/RomanNumeralsTest.java
@@ -2,32 +2,51 @@ package es.upm.fi;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
 public class RomanNumeralsTest {
 
-	@Test
-	public void num5ToV() {
-		
-		assertEquals("V", RomanNumerals.numToRomans(5));
+	private int n;
+	private String result;
+
+	public RomanNumeralsTest(int n, String result) {
+		this.n = n;
+		this.result = result;
 	}
-	
-	@Test
-	public void num3ToIII() {
-		
-		assertEquals("III", RomanNumerals.numToRomans(3));
-	}
-	
-	@Test
-	public void num7ToVII() {
-		
-		assertEquals("VII", RomanNumerals.numToRomans(7));
+
+	@Parameters(name = "Test {index} : RomanNumerals.numToRomans({1}) = {0}")
+	public static Collection<Object[]> myData() {
+		return Arrays.asList(new Object[][] { {-1, "Invalid input number"},
+				{0, "Invalid input number"},
+				{4000, "Invalid input number"},
+				{5, "V"},
+				{3, "III"}, 
+				{7, "VII"},
+				{4, "IV"},
+				{10, "X"},
+				{11, "XI"},
+				{14, "XIV"},
+				{19, "XIX"},
+				{21, "XXI"},
+				{49, "XLIX"},
+				{51, "LI"},
+				{900, "CM"},
+				{1846, "MDCCCXLVI"},
+				{3107, "MMMCVII"},
+				{3444, "MMMCDXLIV"},
+				{3999, "MMMCMXCIX"}});
 	}
 
 	@Test
-	public void num4ToIV() {
-		
-		assertEquals("IV", RomanNumerals.numToRomans(4));
+	public void checkValue(){
+		assertEquals(result, RomanNumerals.numToRomans(n));
 	}
-
 }
