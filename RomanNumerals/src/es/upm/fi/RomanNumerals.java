1cc76dbf8aaccae0872fcd01d8a02ae67dc9f852
k3xr <k3ther@gmail.com>
2015-10-11 14:31:49 +0200

    code optimization

--- RomanNumerals/src/es/upm/fi/RomanNumerals.java
+++ RomanNumerals/src/es/upm/fi/RomanNumerals.java
@@ -1,62 +1,38 @@
 package es.upm.fi;
 
+/**
+ * 
+ * @author oscarf
+ * @since 10-10-2015 
+ *
+ */
 public class RomanNumerals {
 
-	public static String numToRomans(int input) {
-	    if (input < 1 || input > 3999)
-	        return "Invalid Roman Number Value";
-	    String s = "";
-	    while (input >= 1000) {
-	        s += "M";
-	        input -= 1000;        }
-	    while (input >= 900) {
-	        s += "CM";
-	        input -= 900;
-	    }
-	    while (input >= 500) {
-	        s += "D";
-	        input -= 500;
-	    }
-	    while (input >= 400) {
-	        s += "CD";
-	        input -= 400;
-	    }
-	    while (input >= 100) {
-	        s += "C";
-	        input -= 100;
-	    }
-	    while (input >= 90) {
-	        s += "XC";
-	        input -= 90;
-	    }
-	    while (input >= 50) {
-	        s += "L";
-	        input -= 50;
-	    }
-	    while (input >= 40) {
-	        s += "XL";
-	        input -= 40;
-	    }
-	    while (input >= 10) {
-	        s += "X";
-	        input -= 10;
-	    }
-	    while (input >= 9) {
-	        s += "IX";
-	        input -= 9;
-	    }
-	    while (input >= 5) {
-	        s += "V";
-	        input -= 5;
-	    }
-	    while (input >= 4) {
-	        s += "IV";
-	        input -= 4;
-	    }
-	    while (input >= 1) {
-	        s += "I";
-	        input -= 1;
-	    }    
-	    return s;
+	static String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	static int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+	/**
+	 * 
+	 * @param originalNumber integer to be converted to Roman
+	 * @return String with the Roman representation of the originalNumber
+	 */
+	public static String numToRomans(int originalNumber) {
+
+		if (originalNumber < 1 || originalNumber > 3999){
+			return "Invalid input number";
+		}
+
+		String romanNumber = "";		
+		int numLetters = 0;
+
+		for (int i = 0; i < romans.length; i++) {
+			numLetters = originalNumber / numbers[i];
+			originalNumber %= numbers[i];
+			for(int j = 0; j < numLetters; j++){
+				romanNumber += romans[i];
+			}
+		}
+
+		return romanNumber;
 	}
 }
