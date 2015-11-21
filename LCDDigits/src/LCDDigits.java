ba1c4e4e5d3bc5b0a6740eeeddf6302c32429a32
k3xr <k3ther@gmail.com>
2015-11-21 16:47:30 +0100

    Test fixes

--- LCDDigits/src/LCDDigits.java
+++ LCDDigits/src/LCDDigits.java
@@ -1,46 +1,46 @@
 
 public class LCDDigits {
 
-	String row0 = "";
-	String row1 = "";
-	String row2 = "";
-	
+	private String row0 = "";
+	private String row1 = "";
+	private String row2 = "";
+
 	String row00 = " _ ";
 	String row01 = "| |";
 	String row02 = "|_|";
-	
+
 	String row10 = "   ";
 	String row11 = "  |";
 	String row12 = "  |";
-	
+
 	String row20 = " _ ";
 	String row21 = " _|";
 	String row22 = "|_ ";
-	
+
 	String row30 = " _ ";
 	String row31 = " _|";
 	String row32 = " _|";
-	
+
 	String row40 = "   ";
 	String row41 = "|_|";
 	String row42 = "  |";
-	
+
 	String row50 = " _ ";
 	String row51 = "|_ ";
 	String row52 = " _|";
-	
+
 	String row60 = " _ ";
 	String row61 = "|_ ";
 	String row62 = "|_|";
-	
+
 	String row70 = " _ ";
 	String row71 = "  |";
 	String row72 = "  |";
-	
+
 	String row80 = " _ ";
 	String row81 = "|_|";
 	String row82 = "|_|";
-	
+
 	String row90 = " _ ";
 	String row91 = "|_|";
 	String row92 = "  |";
@@ -62,55 +62,58 @@ public class LCDDigits {
 	}
 
 	private void digitToStringRepresentation(int digit){
-		if(digit == 0){
+
+		switch(digit){
+		case 0:
 			row0 = row00+row0;
 			row1 = row01+row1;
 			row2 = row02+row2;
-		}
-		else if (digit == 1){
+			break;
+		case 1:
 			row0 = row10+row0;
 			row1 = row11+row1;
 			row2 = row12+row2;
-		}
-		else if (digit == 2){
+			break;
+		case 2:
 			row0 = row20+row0;
 			row1 = row21+row1;
 			row2 = row22+row2;
-		}
-		else if (digit == 3){
+			break;
+		case 3:
 			row0 = row30+row0;
 			row1 = row31+row1;
 			row2 = row32+row2;
-		}
-		else if (digit == 4){
+			break;
+		case 4:
 			row0 = row40+row0;
 			row1 = row41+row1;
 			row2 = row42+row2;
-		}
-		else if (digit == 5){
+			break;
+		case 5:
 			row0 = row50+row0;
 			row1 = row51+row1;
 			row2 = row52+row2;
-		}
-		else if (digit == 6){
+			break;
+		case 6:
 			row0 = row60+row0;
 			row1 = row61+row1;
 			row2 = row62+row2;
-		}
-		else if (digit == 7){
+			break;
+		case 7:
 			row0 = row70+row0;
 			row1 = row71+row1;
 			row2 = row72+row2;
-		}
-		else if (digit == 8){
+			break;
+		case 8:
 			row0 = row80+row0;
 			row1 = row81+row1;
 			row2 = row82+row2;
-		}
-		else if (digit == 9){
+			break;
+		case 9:
 			row0 = row90+row0;
 			row1 = row91+row1;
 			row2 = row92+row2;
+			break;
 		}
 	}
 }
--- LCDDigits/test/LCDDigitsTest.java
+++ LCDDigits/test/LCDDigitsTest.java
@@ -7,39 +7,51 @@ public class LCDDigitsTest {
 	@Test
 	public void number0() {
 		LCDDigits l = new LCDDigits();
-		assertEquals(" _ \n| |\n|_|\n", l.stringRepresentation(0));
+		assertEquals(l.row00+"\n"+l.row01+"\n"+l.row02+"\n", l.stringRepresentation(0));
 	}
 	@Test
 	public void number7() {
 		LCDDigits l = new LCDDigits();
-		assertEquals(" _ \n  |\n  |\n", l.stringRepresentation(7));
+		assertEquals(l.row70+"\n"+l.row71+"\n"+l.row72+"\n", l.stringRepresentation(7));
 	}
 	@Test
 	public void number9() {
 		LCDDigits l = new LCDDigits();
-		assertEquals(" _ \n|_|\n  |\n", l.stringRepresentation(9));
+		assertEquals(l.row90+"\n"+l.row91+"\n"+l.row92+"\n", l.stringRepresentation(9));
 	}
 	@Test
 	public void number2() {
 		LCDDigits l = new LCDDigits();
-		assertEquals(" _ \n _|\n|_ \n", l.stringRepresentation(2));
+		assertEquals(l.row20+"\n"+l.row21+"\n"+l.row22+"\n", l.stringRepresentation(2));
 	}
 	@Test
 	public void number12() {
 		LCDDigits l = new LCDDigits();
-		assertEquals("    _ \n  | _|\n  ||_ \n", l.stringRepresentation(12));
+		assertEquals(l.row10+l.row20+"\n"+
+				l.row11+l.row21+"\n"+
+				l.row12+l.row22+"\n", l.stringRepresentation(12));
 	}
-	
+
 	@Test
 	public void number240() {
-		LCDDigits l = new LCDDigits();
-		System.out.println(l.stringRepresentation(240));
-		System.out.println(l.row20+l.row40+l.row00+"\n"+
-				l.row21+l.row41+l.row01+"\n"+
-				l.row22+l.row42+l.row02+"\n");
+		LCDDigits l = new LCDDigits();		
 		assertEquals(l.row20+l.row40+l.row00+"\n"+
 				l.row21+l.row41+l.row01+"\n"+
 				l.row22+l.row42+l.row02+"\n", l.stringRepresentation(240));
+	}
+
+	@Test
+	public void number000() {
+		LCDDigits l = new LCDDigits();		
+		assertEquals(l.row00+"\n"+l.row01+"\n"+l.row02+"\n", l.stringRepresentation(000));
+	}
+
+	@Test
+	public void number9999() {
+		LCDDigits l = new LCDDigits();		
+		assertEquals(l.row90+l.row90+l.row90+l.row90+"\n"+
+				l.row91+l.row91+l.row91+l.row91+"\n"+
+				l.row92+l.row92+l.row92+l.row92+"\n", l.stringRepresentation(9999));
 	}
 
 }
